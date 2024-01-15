b5705c85697fc7198380250412cc9ebd5252ca60
Seema <simapoudel6@gmail.com>
2024-01-15 16:00:28 +0545

    update

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -15,7 +15,7 @@ const Board = () => {
             [1, 4, 7],
             [2, 5, 8],
             [0, 4, 8], 
-            [2, 4, 6]  
+            [2, 4, 6],  
         ];
         
 
@@ -25,13 +25,17 @@ const Board = () => {
                 return state[a];
             }
         }
+        //draw condition
+        if (state.every((square) => square !== null)) {
+            return 'Draw';
+        }
         return false;
     };
     
     const isWinner= checkWinner();
 
     const handleClick = (index)=>{
-
+        // Check if the square is already filled or there's a winner
         if (state[index] !== null || isWinner) {
             return;
         }
@@ -44,30 +48,33 @@ const Board = () => {
 
     return(
         <div className="board-container">
-            {isWinner ? 
-            (<>{isWinner} won the game. </>):
-            (
-            <>
-            <div className="row">
-                <Square onClick={()=>handleClick(0)} value={state[0]}/>
-                <Square onClick={()=>handleClick(1)} value={state[1]}/>
-                <Square onClick={()=>handleClick(2)} value={state[2]}/>
-            </div>
-            <div className="row">
-                <Square onClick={()=>handleClick(4)} value={state[4]}/> 
-                <Square onClick={()=>handleClick(3)} value={state[3]}/>
-                <Square onClick={()=>handleClick(5)} value={state[5]}/>
-            </div>
-            <div className="row">
-                <Square onClick={()=>handleClick(6)}value={state[6]}/>
-                <Square onClick={()=>handleClick(7)}value={state[7]}/>
-                <Square onClick={()=>handleClick(8)}value={state[8]}/>
-            </div>
-            </>
-            )
-            }
-
+            {isWinner ? (
+                isWinner === 'Draw' ? (
+                    <>It's a draw!</>
+                ) : (
+                <>{isWinner} won the game. </>
+                )
+            ) : (
+                <>
+                    <div className="row">
+                        <Square onClick={() => handleClick(0)} value={state[0]} />
+                        <Square onClick={() => handleClick(1)} value={state[1]} />
+                        <Square onClick={() => handleClick(2)} value={state[2]} />
+                    </div>
+                    <div className="row">
+                        <Square onClick={() => handleClick(3)} value={state[3]} />
+                        <Square onClick={() => handleClick(4)} value={state[4]} />
+                        <Square onClick={() => handleClick(5)} value={state[5]} />
+                    </div>
+                    <div className="row">
+                        <Square onClick={() => handleClick(6)} value={state[6]} />
+                        <Square onClick={() => handleClick(7)} value={state[7]} />
+                        <Square onClick={() => handleClick(8)} value={state[8]} />
+                    </div>
+                </>
+            )}
         </div>
+    
     )
 }
 
